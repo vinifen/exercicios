34c550ceaffe4da352c087c7fa4aebcb5fa1c28b
vinifen <ferreiranivicius@gmail.com>
2024-06-11 21:48:53 -0300

    Mais em matrizes bidimencionais

--- exercicios-fund-programacao-2p/matriz-bidimencional/MatrizAleatoria.ts
+++ exercicios-fund-programacao-2p/matriz-bidimencional/MatrizAleatoria.ts
@@ -4,26 +4,26 @@ matriz inteira de l linhas e c colunas, sendo l e c fornecidos ao construtor. */
 
 export default class MatrizAleatoria {
     public matriz: number[][];
+    public l: number;
+    public c: number;
 
     /*Exercício 2
     Faça com que essa matriz seja inicializada, no construtor, com inteiros aleatórios entre 0 e 99. A
     implementação dos demais métodos é descrita na sequência. Os parâmetros e o retorno precisam
     ser inferidos.*/
     constructor(l: number, c: number) {
-        this.matriz = this.criarMatrizAleatoria(l, c);
-    }
+        this.l = l;
+        this.c = c;
+        this.matriz = [];
 
-    public criarMatrizAleatoria(l: number, c: number): number[][] {
-        let matriz: number[][] = [];
-        for (let i = 0; i < l; i++) {
+        for (let i = 0; i < this.l; i++) {
             let linha: number[] = [];
-            for (let j = 0; j < c; j++) {
+            for (let j = 0; j < this.c; j++) {
                 linha.push(Math.floor(Math.random() * 100));
             }
-            matriz.push(linha);
+            this.matriz.push(linha);
         }
-        console.log(matriz[0].length);
-        return matriz;
+        console.log(this.matriz[0].length);
     }
 
     /*Exercício 3
@@ -41,21 +41,46 @@ export default class MatrizAleatoria {
     com a coluna.*/
 
     public somaLinhaColuna(){
-        let contL = 0;
-        let contC = 0;
-        for(let i = 0; i < this.matriz.length; i++){
-            for(let y = 0; y < this.matriz[i].length; y++){
-                let linha = this.matriz[i][y];
-                contL += linha;
-            }
-        }
+        
+    }
 
-        for(let i = 0; i < this.matriz.length; i++){
-            let coluna = this.matriz[i];
-            for(let y = 0; y < this.matriz.length; y++){
-                let linha = this.matriz[i][y];
-            }
+    /*Exercício 5
+    multiplicaPor()
+    Multiplica a própria matriz por um inteiro n, fornecido como parâmetro.*/
+
+    public multiplicaPor(){
+
+    }
+
+    /*Exercício 6
+    somaCom()
+    Retorna o resultado da operação M + N, sendo N fornecida como parâmetro. O retorno deve ser
+    number[][].*/
+
+    public somaCom(n: number[][]): number[][]{
+        if(this.matriz.length != n.length)
+            return [];
+        
+        if(this.matriz[0].length != n[0].length)
+            return [];
+        let aux: number [][] = [];
+        for(let i = 0; i < this.l; i++){
+            aux[i] = [];
+
+            for(let y = 0; y < this.c; y++)
+                aux[i][y] = this.matriz[i][y] + n[i][y];
         }
+        return aux;
+    }
+
+    /*Exercício 7
+    binaria()
+    Retorna uma matriz binária de mesma ordem, que possui zero nas posições em que a matriz original
+    tinha elementos pares e um nas posições em que havia elementos ímpares. O retorno deve ser
+    number[][].*/
+
+    public binaria(){
+
     }
     
 }
--- exercicios-fund-programacao-2p/matriz-bidimencional/index.ts
+++ exercicios-fund-programacao-2p/matriz-bidimencional/index.ts
@@ -3,5 +3,14 @@ import MatrizAleatoria from "./MatrizAleatoria";
 
 let prompt = PromptSync();
 let matriz: MatrizAleatoria = new MatrizAleatoria(5, 5);
-
+let matrizExemplo: number [][] = 
+[
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5]
+]
 console.log(matriz);
+
+console.log(matriz.somaCom(matrizExemplo));
